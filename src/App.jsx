5081995acebc3b123c2a48f3e05dc407e1eb3d57
RaghavOG <04raghavsingla28@gmail.com>
2025-04-20 10:04:40 +0530

    Added Notes Thing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import DocumentLibrary from "./components/DocumentLibrary";
 import ExamSchedule from "./components/ExamSchedule";
 import HomeDashboard from "./components/Home";
+import NotesLibrary from "./components/NotesLibrary";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HomeDashboard />
       <DocumentLibrary />
       <ExamSchedule />
+      <NotesLibrary />
     </>
   );
 }
